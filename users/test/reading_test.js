529eee253ed166a23e4228ad0bd58a48fb848bb6
Murali Nutalapati <info@muraliprashanth.me>
2017-09-07 16:48:29 +0530

    retrives user from mongodb

---
+++ users/test/reading_test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+// retrieve records from mongodb.
+describe('reading users from db', () => {
+    let murali;
+
+    beforeEach((done) => {
+        murali = new User({
+            name: 'murali',
+        });
+        murali.save().then(() => done());
+    });
+
+    // done = tells mocha that this test takes sometime and call's done
+    it('finds all of users with name murali', (done) => {
+        User.find({name:'murali'}).then((users) => {
+            console.log(users);
+            done();
+        })
+    });
+});
