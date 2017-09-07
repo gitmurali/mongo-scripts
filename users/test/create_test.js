446ea2c62163e65c2b3422f9cdcbc78df3a39907
Murali Nutalapati <info@muraliprashanth.me>
2017-09-07 15:51:29 +0530

    updates the mongoose promise library to es6 promise and adds beforeEach done callback

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -3,10 +3,16 @@ const User = require('../src/user');
 
 
 describe('Creating records', () => {
-    it('save a user', () => {
+    it('save a user', (done) => {
         const murali = new User({
             name: 'Murali'
         });
-        murali.save();
+
+        murali.save().then(()=>{
+            // isNew is a mongoose helper to find out if user record is saved or not if it's saved
+            // then isNew is set to false
+            assert(!murali.isNew);
+            done();
+        });
     });
 });
--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
+// hey mongoose use node env es6 implemented Promise library
+mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://localhost/users_test', { useMongoClient: true });
-mongoose.connection.once('open', () => {
-    console.log('good to go!!');
-}).on('error', (error) =>{console.warn('warning', error)});
+mongoose.connection.once('open', () => {})
+    .on('error', (error) =>{console.warn('warning', error)});
 
 beforeEach((done) => {
     mongoose.connection.collections.users.drop(() => {
