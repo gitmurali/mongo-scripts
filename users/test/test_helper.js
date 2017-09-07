3d50ef3f1e0e1d2fe8ab071382bcb6010fdb930f
Murali Nutalapati <info@muraliprashanth.me>
2017-09-07 16:02:35 +0530

    comments update

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose');
 // hey mongoose use node env es6 implemented Promise library
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/users_test', { useMongoClient: true });
-mongoose.connection.once('open', () => {})
-    .on('error', (error) =>{console.warn('warning', error)});
+// the done callback tells mocha to wait until a connection is open to mongo.
+before((done)=> { // this will be called once for entire test suite
+    mongoose.connect('mongodb://localhost/users_test', { useMongoClient: true });
+    mongoose.connection.once('open', () => {
+        done();
+    }).on('error', (error) =>{console.warn('warning', error)});
+});
 
-beforeEach((done) => {
+beforeEach((done) => { // this will be called before each test case.
     mongoose.connection.collections.users.drop(() => {
           done();
     });
