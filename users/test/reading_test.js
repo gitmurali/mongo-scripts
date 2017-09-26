53a0f5436da8697ee1fdcde24ac9e9a7602245f7
Murali Nutalapati <info@muraliprashanth.me>
2017-09-26 16:01:03 +0530

    Adds the skip, limit, and sort for mongo

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -3,13 +3,22 @@ const User = require('../src/user');
 
 // retrieve records from mongodb.
 describe('reading users from db', () => {
-    let murali;
+    let murali, prashanth, bobby, candy, zach;
 
     beforeEach((done) => {
-        murali = new User({
-            name: 'murali',
-        });
-        murali.save().then(() => done());
+        murali = new User({name: 'murali'});
+        prashanth = new User({name: 'prashanth'});
+        bobby = new User({name: 'bobby'});
+        candy = new User({name: 'candy'});
+        zach = new User({name: 'zach'});
+        Promise.all([
+          murali.save(),
+          prashanth.save(),
+          bobby.save(),
+          candy.save(),
+          zach.save()]).then(() => {
+            done();
+        })
     });
 
     // done = tells mocha that this test takes sometime and call's done
@@ -25,5 +34,11 @@ describe('reading users from db', () => {
             assert(user.name === 'murali');
             done();
         })
+    });
+
+    it('can skip and limit the result set and also sort by name in ascending', () => {
+        User.find({}).skip(1).limit(3).sort({name:1}).then((users) => {
+            console.log(users);
+        });
     });
 });
